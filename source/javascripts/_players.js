d4f231ca8da77faadfcddd8f0d84eb542fd70b02
endcerro <gmx1pt@gmail.com>
2019-07-01 22:04:51 +0200

    copy from local 2018 version

--- source/javascripts/_players.js
+++ source/javascripts/_players.js
@@ -2,13 +2,13 @@ var players = [
   {
     id: 0,
     avatar: 'player0.gif',
-    name: 'Crewer',
+    name: 'player0',
     screenname: 'xX-Player-Xx'
   },
   {
     id: 1,
     avatar: 'player1.gif',
-    name: 'Noar',
+    name: 'player1',
     screenname: 'xX-Player-Xx'
   }
 ]
